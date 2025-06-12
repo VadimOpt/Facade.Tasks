e7b218a16e8918df9de8e613b95427f7b3f768e7
vadim <vadimdidur20@gmail.com>
2025-06-12 13:41:06 +0300

    Make TimeoutCollection Map-compatible

--- JavaScript/2-class.js
+++ JavaScript/2-class.js
@@ -13,31 +13,68 @@ class TimeoutCollection {
     const timeout = setTimeout(() => {
       this.delete(key);
     }, this.timeout);
-    timeout.unref();
+    if (typeof timeout.unref === 'function') timeout.unref();
     this.collection.set(key, value);
     this.timers.set(key, timeout);
+    return this;
   }
 
   get(key) {
     return this.collection.get(key);
   }
 
+  has(key) {
+    return this.collection.has(key);
+  }
+
   delete(key) {
     const timer = this.timers.get(key);
     if (timer) {
       clearTimeout(timer);
-      this.collection.delete(key);
       this.timers.delete(key);
+      return this.collection.delete(key);
     }
+    return false;
+  }
+
+  clear() {
+    for (const timer of this.timers.values()) {
+      clearTimeout(timer);
+    }
+    this.timers.clear();
+    this.collection.clear();
+  }
+
+  size() {
+    return this.collection.size;
   }
 
   toArray() {
     return [...this.collection.entries()];
   }
+
+  forEach(callbackfn, thisArg) {
+    return this.collection.forEach(callbackfn, thisArg);
+  }
+
+  keys() {
+    return this.collection.keys();
+  }
+
+  values() {
+    return this.collection.values();
+  }
+
+  entries() {
+    return this.collection.entries();
+  }
+
+  [Symbol.iterator]() {
+    return this.collection[Symbol.iterator]();
+  }
 }
 
 // Usage
-
 const hash = new TimeoutCollection(1000);
 hash.set('uno', 1);
 console.dir({ array: hash.toArray() });
